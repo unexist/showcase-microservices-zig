b100b8395f5de4d5932730bf041f44aecb07f3e7
Christoph Kappel <christoph@unexist.dev>
2024-03-25 12:15:24 +0100

    Added repository interface

---
+++ todo-service-zap/src/domain/repository.zig
@@ -0,0 +1,38 @@
+//
+// @package Showcase-Microservices-Zig
+//
+// @file Todo repository
+// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+// @version $Id$
+//
+// This program can be distributed under the terms of the Apache License v2.0.
+// See the file LICENSE for details.
+//
+
+const Todos = @import("todo.zig");
+
+const Repository = struct {
+    // Open connection to database                                                                                                                                                                                                                                                                                [2/1876]
+    open: fn (*Repository, []const u8) void,
+
+    // Get all todos stored by this repository
+    getTodos: fn (*Repository, ?[]Todo) void,
+
+    // Create new todo based on given values
+    createTodo: fn (*Repository, ?Todo) void,
+
+    // Get todo entry with given id
+    getTodo: fn (*Repository, usize) ?Todo,
+
+    // Update todo entry with given id
+    updateTodo: fn (*Repository, ?Todo) void,
+
+    // Delete todo entry with given id
+    deleteTodo: fn (*Repository, usize) void,
+
+    // Clear table
+    clear: fn (*Repository) void,
+
+    // Close database connection
+    close: fn (*Repository) void,
+};
